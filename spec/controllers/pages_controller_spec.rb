2990c7beb0c7cee827752c74cbaf6f70bc79070b
= <jerem.gerard@gmail.com>
2015-03-19 12:51:50 +0100

    Fini avec les pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe PagesController, type: :controller do
       get :home
       expect(response).to have_http_status(:success)
     end
+    it "Must have the good title" do
+      get :home
+      expect(response).to have_selector("title",
+        :content => "Simple App du tutoriel Ruby On Rails | Accueil")
+    end
   end
 
   describe "GET #contact" do
@@ -14,6 +19,26 @@ RSpec.describe PagesController, type: :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+    it "Must have the good title" do
+      get :contact
+      expect(response).to have_selector("title",
+        :content => "Simple App du tutoriel Ruby On Rails | Contact")
+    end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+    it "Must have the good title" do
+      get :about
+      expect(response).to have_selector("title",
+        :content => "Simple App du tutoriel Ruby On Rails | A propos")
+    end
+  end
+
+
+
 end
+ 
